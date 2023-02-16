d80e8aa9174de7d2eec9240623b8cc88f285e841
Kyle John O'Connor <kyle.oconnor@angloamerican.com>
2023-02-16 14:12:22 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -13,17 +13,22 @@ const Person = mongoose.model("Person", personSchema);
 
 mongoose.connect(process.env.MONGO_URI);
 
-const createAndSavePerson = (done) => {
-  var newPerson = new Person({
-    name: "Bepp Boop",
-    age: 123,
-    favoriteFoods: ['Dust', 'Sadness', 'Pizza']
+// const createAndSavePerson = (done) => {
+//   var newPerson = new Person({ name: "Bepp Boop", age: 123, favoriteFoods: ['Dust', 'Sadness', 'Pizza']});
+
+//   newPerson.save(function(err,data){
+//     if(err) return console.error(err);
+//     done(null, data);
+//   });
+// };
+
+var createAndSavePerson = function(done) {
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
   });
-  newPerson.save(function(err,data){
-    if(err) return console.error(err);
-    done(null, data);
-  });
-  
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
