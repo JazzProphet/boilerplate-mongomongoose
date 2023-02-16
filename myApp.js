a707802555d420141f5a3fba8ba137f61df45412
Kyle John O'Connor <kyle.oconnor@angloamerican.com>
2023-02-16 13:12:06 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 let Person;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
