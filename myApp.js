5b70c4871b4c1f86f34ae19e93eb588a982783ab
Kyle John O'Connor <kyle.oconnor@angloamerican.com>
2023-02-17 08:59:36 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -13,23 +13,23 @@ const Person = mongoose.model("Person", personSchema);
 
 mongoose.connect(process.env.MONGO_URI);
 
-var createAndSavePerson = (done) => {
-  var newPerson = new Person({ name: "Bepp Boop", age: 123, favoriteFoods: ['Dust', 'Sadness', 'Pizza']});
+// var createAndSavePerson = (done) => {
+//   var newPerson = new Person({ name: "Bepp Boop", age: 123, favoriteFoods: ['Dust', 'Sadness', 'Pizza']});
 
-  newPerson.save(function(err,data){
-    if(err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// var createAndSavePerson = function(done) {
-//   var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
-
-//   janeFonda.save(function(err, data) {
-//     if (err) return console.error(err);
-//     done(null, data)
+//   newPerson.save(function(err,data){
+//     if(err) return console.error(err);
+//     done(null, data);
 //   });
 // };
+
+var createAndSavePerson = function(done) {
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
